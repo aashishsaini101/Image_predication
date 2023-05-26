3d00e8588c1ef34d870f059ea5d99c487b1926ad
aashishsaini101 <96624892+aashishsaini101@users.noreply.github.com>
2023-05-26 11:36:25 +0530

    Add files via upload

---
+++ project101.py
@@ -0,0 +1,31 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
+from keras import datasets, layers, models
+from keras.datasets import cifar10
+
+
+(training_images, training_labels), (testing_images, testing_labels) = datasets.cifar10.load_data()
+training_images, testing_images = training_images / 255, testing_images / 255
+
+class_names = ['Plane', 'Car', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'ship', 'Truck']
+
+for i in range(16):
+    plt.subplot(4,4,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(training_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[training_labels[i][0]])
+plt.show()
+
+
+model = models.load_model('image_classifier.model')
+
+img = cv.imread('s1.jpg')
+img = cv.cvtColor(img,cv.COLOR_BGR2RGB)
+
+plt.imshow(img, cmap=plt.cm.binary)
+
+prediction = model.predict(np.array([img]) / 255)
+index = np.argmax(prediction)
+print(f'predication is {class_names[index]}')
